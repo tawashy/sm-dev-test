b423a6d1789537bb87d7dfeb41af97ca60b8b4e9
Yaser I. Tawash <yaser.tawash@gmail.com>
2023-12-21 10:32:17 +0300

    chore: comment out

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// "MongoConnectionString": "mongodb://myserv:SKiEI6tRk55K4r3dwUCF5viVEWZmJsSJ9lUVHUM1xgVRuoGQqilhCTqquOPSPyaw7x7DUAen5f4eACDbs9d9Vg==@myserv.mongo.cosmos.azure.com:10244/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@myserv@"
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
